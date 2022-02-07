e4b11fa6427453d926ca282a2ca5647bbefa1b0e
jaumecf <caf@paucasesnovescifp.cat>
2022-02-07 11:22:07 +0100

    Estructura amb variable currentIndex

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scan/screens/home_screen.dart';
+import 'package:qr_scan/screens/mapa_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +8,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen(),
+        'mapa': (_) => MapaScreen(),
+      },
+      theme: ThemeData(
+        // No es pot emprar colorPrimary des de l'actualització de Flutter
+        colorScheme: ColorScheme.light().copyWith(
+          primary: Colors.deepPurple,
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.deepPurple,
         ),
       ),
     );
