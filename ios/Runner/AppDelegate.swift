f78c0eec6e9474c5ba88969960560732f6ae7c00
jaumecf <caf@paucasesnovescifp.cat>
2022-02-15 01:02:53 +0100

    API+GoogleMaps importat i funcionament inicial

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCzgZ1wwSc_af-ogAeDhRgE9kY-KFgogQI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screens/mapa_screen.dart
+++ lib/screens/mapa_screen.dart
@@ -1,12 +1,25 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../models/scan_model.dart';
 
-class MapaScreen extends StatelessWidget {
+class MapaScreen extends StatefulWidget {
   const MapaScreen({Key? key}) : super(key: key);
 
   @override
+  State<MapaScreen> createState() => _MapaScreenState();
+}
+
+class _MapaScreenState extends State<MapaScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
   Widget build(BuildContext context) {
+    final CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
 
@@ -14,7 +27,13 @@ class MapaScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Mapa'),
       ),
-      body: Center(child: Text('Home Screen')),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
